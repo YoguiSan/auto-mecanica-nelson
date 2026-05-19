e7b1630823031bd19a8058126f3262f3eceb6afc
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2026-05-19 12:32:14 -0300

    test(Mocks): :white_check_mark: fileMock

--- __mocks__/fileMock.ts
+++ __mocks__/fileMock.ts
@@ -1,8 +1,8 @@
+import React from 'react';
+
 const mock = 'test-file-stub';
 export default mock;
 export const ReactComponent = (props: any) => {
-	// return a simple element for svg imports used in components
-	const { alt = '', ...rest } = props || {};
-	// @ts-ignore JSX
-	return (<img alt={alt} {...rest} />);
+  const { alt = '', ...rest } = props || {};
+  return React.createElement('img', { alt, ...rest });
 };
