5ed69cb831f584c7431889d3a30fc9f41f216952
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2026-06-17 13:26:37 -0300

    Delete packages/vehicle-system-api directory

--- packages/vehicle-system-api/scripts/linux/run.sh
+++
@@ -1,4 +0,0 @@
-sudo docker build -t vehicle-data-api packages/vehicle-system-api --progress=plain
-sudo docker run -p 8080:8080 \
-  -e ConnectionStrings__DefaultConnection="server=host.docker.internal;..." \
-  vehicle-data-api
