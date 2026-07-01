d73f8f7157bd89177cf45f8a2219261fbba94268
Nelson Salles de Barros Filho <nelsonsbf@protonmail.com>
2026-07-01 17:43:17 -0300

    feat: CircleCI config

--- src/components/WebComponentLoader.tsx
+++ src/components/WebComponentLoader.tsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react';
 export default function WebComponentLoader() {
   useEffect(() => {
     import('ethyl-ui');
-    import('ethyl-ui/index.css');
   }, []);
 
   return null;
