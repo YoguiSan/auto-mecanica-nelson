b9913cc506c56e6b251b864ef1039daf854c1c89
Nelson Salles de Barros Filho <nelsonsbf@outlook.com>
2026-09-16 15:35:52 -0300

    build(scripts): :bug: Caminho do YAML do frontend

--- scripts/deployment/frontend.sh
+++ scripts/deployment/frontend.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-kubectl apply -f devops/frontend/deployment.yaml
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+kubectl apply -f "$SCRIPTPATH../../devops/frontend/deployment.yaml"
